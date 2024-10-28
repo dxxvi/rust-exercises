d0e3689449b21f8ea48344764eb0bac93b62f3d9
Thai <dxxvi@yahoo.com>
2024-10-28 17:23:50 -0400

    ...

--- exercises/04_traits/02_orphan_rule/src/lib.rs
+++ exercises/04_traits/02_orphan_rule/src/lib.rs
@@ -4,8 +4,10 @@
 //  Look at the compiler error to get familiar with what it looks like.
 //  Then delete the code below and move on to the next exercise.
 
+/*// Only traits defined in the current crate can be implemented for arbitrary types
 impl PartialEq for u32 {
     fn eq(&self, _other: &Self) -> bool {
         todo!()
     }
 }
+*/
